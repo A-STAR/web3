50f53fd283780331fb676d5d513eac34de148ed7
A-STAR <andreysuperstar@gmail.com>
2025-02-18 17:47:40 +0300

    feat(Metamask React): request Metamask accounts

--- metamask-react/src/App.tsx
+++ metamask-react/src/App.tsx
@@ -8,6 +8,9 @@ function App() {
 	const [provider, setProvider] = useState<string | null>(null);
 	const [chainId, setChainId] = useState<string | null>(null);
 	const [latestBlock, setLatestBlock] = useState<string | null>(null);
+	const [accountButtonDisabled, setAccountButtonDisabled] = useState<boolean>(false);
+	const [accounts, setAccounts] = useState<string[] | null>(null);
+	const [connectedAccount, setConnectedAccount] = useState<string | null>(null);
 	useEffect(() => {
 		// ensure that there is an injected the Ethereum provider
 		if (window.ethereum) {
@@ -22,6 +25,7 @@ function App() {
 		} else {
 			// no Ethereum provider - instruct user to install MetaMask
 			setWarning('Please install MetaMask');
+			setAccountButtonDisabled(true);
 		}
 	}, []);
 
@@ -53,6 +57,23 @@ function App() {
 		getChainId();
 		getLatestBlock();
 	}, [web3]);
+
+	// click event for "Request MetaMask Accounts" button
+	async function requestAccounts() {
+		if (web3 === null) {
+			return;
+		}
+
+		// request accounts from MetaMask
+		await window.ethereum.request({ method: 'eth_requestAccounts' });
+		document.getElementById('request-accounts')?.remove();
+
+		// get list of accounts
+		const allAccounts = await web3.eth.getAccounts();
+		setAccounts(allAccounts);
+		// get the first account and populate placeholder
+		setConnectedAccount(`Account: ${allAccounts[0]}`);
+	}
 	return (
 		<>
 			<p id="warn" style={{ color: 'red' }}>
@@ -61,6 +82,16 @@ function App() {
 			<p id="provider">{provider}</p>
 			<p id="chain-id">{chainId}</p>
 			<p id="latest-block">{latestBlock}</p>
+			<p id="connected-account">{connectedAccount}</p>
+			<div>
+				<button
+					onClick={() => requestAccounts()}
+					id="request-accounts"
+					disabled={accountButtonDisabled}
+				>
+					Request MetaMask Accounts
+				</button>
+			</div>
 		</>
 	);
 }
