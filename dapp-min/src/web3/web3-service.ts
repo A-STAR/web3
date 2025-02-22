3032df2f143576318bea1a60a2be6e922087d3a1
A-STAR <andreysuperstar@gmail.com>
2025-02-23 00:00:33 +0300

    refactor(dApp min): optimize prod build

--- dapp-min/src/web3/web3-service.ts
+++ dapp-min/src/web3/web3-service.ts
@@ -1,11 +1,12 @@
-import { type EIP1193Provider, Web3, type Web3APISpec } from 'web3';
+import { Web3Eth } from 'web3-eth';
+import type { EIP1193Provider, Web3APISpec } from 'web3-types';
 
 const provider: EIP1193Provider<Web3APISpec> | undefined = window.ethereum;
 if (provider !== undefined) {
-  provider.on("chainChanged", () => window.location.reload());
+	provider.on("chainChanged", () => window.location.reload());
 }
 
-const web3: Web3 = provider === undefined ? new Web3() : new Web3(provider);
+const web3: Web3Eth = provider === undefined ? new Web3Eth() : new Web3Eth(provider);
 const Web3Service = { provider, web3 };
 
 export default Web3Service;
