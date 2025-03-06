1b72f8ab81cfbb69c729c087df44006bcc9f26be
A-STAR <andreysuperstar@gmail.com>
2025-03-07 00:19:15 +0300

    feat(web3-eth): legacy transaction

---
+++ eth/legacy.ts
@@ -0,0 +1,28 @@
+import { Web3 } from 'web3';
+
+const web3 = new Web3('http://localhost:8545');
+
+async function test() {
+	const privateKey = 'YOUR PRIVATE KEY HERE';
+	// add private key to wallet to have auto-signing transactions feature
+	const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+	web3.eth.accounts.wallet.add(account);
+
+	// create transaction object
+	const tx = {
+		from: account.address,
+		to: '0x27aa427c1d668ddefd7bc93f8857e7599ffd16ab',
+		value: '0x1',
+		gas: BigInt(21000),
+		gasPrice: await web3.eth.getGasPrice(),
+		type: BigInt(0) // <- specify type
+	};
+
+	// send transaction
+	const receipt = await web3.eth.sendTransaction(tx);
+
+	console.log('Receipt:', receipt);
+}
+(async () => {
+	await test();
+})();
